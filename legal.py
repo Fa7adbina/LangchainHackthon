48f5a1ab266aa4b295aa80af9fa94a6726b64c7a
Fa7adbina <88969472+Fa7adbina@users.noreply.github.com>
2023-06-17 23:06:28 +0300

    Add files via upload

---
+++ legal.py
@@ -0,0 +1,33 @@
+# import libraries
+import os
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+
+# Our Function to take texst
+
+def Legal_Researcher(input_variable,language):
+  input_variable = input_variable+" . " +language
+  # create a new openai api key
+  os.environ["OPENAI_API_KEY"] = "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"
+  # set up openai api key
+  openai_api_key = os.environ.get('OPENAI_API_KEY')
+
+  # Load in some tools to use
+  os.environ["SERPAPI_API_KEY"] = "581b459d9d1c771f55fd6468ff513ad89f127a48e80983c897bfb33d708d37fc"
+
+  # set up openai api key
+  openai_api_key = os.environ.get('SERPAPI_API_KEY')
+ 
+  # create a model
+  llm = OpenAI(temperature = 0.9)
+
+  tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+  agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+  Output_For_Function =agent.run(input_variable) 
+ 
+
+  return Output_For_Function
